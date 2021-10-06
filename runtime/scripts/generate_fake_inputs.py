6d280e9639b1b8d63701705a68f80e5ca4f33f75
Robert Gibboni <robert@drivendata.org>
2021-10-06 15:55:38 -0400

    Basic generate fake data

--- runtime/scripts/generate_fake_inputs.py
+++ runtime/scripts/generate_fake_inputs.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+from PIL import Image
 import typer
 
 
 def main(output_dir: Path, seed: int = 42):
-    pass
+    for image in range(10):
+        Image.new("RGB", (10, 10)).save(output_dir / f"{image}.tif")
 
 
 if __name__ == "__main__":
