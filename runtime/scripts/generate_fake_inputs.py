5285f3dd60d9b98e930ea5d87114e436fbcbe0a4
Robert Gibboni <robert@drivendata.org>
2021-11-04 17:34:49 -0400

    Update generate fake inputs to make tifs with single bands

--- runtime/scripts/generate_fake_inputs.py
+++ runtime/scripts/generate_fake_inputs.py
@@ -1,12 +1,18 @@
 from pathlib import Path
 
+import numpy as np
 from PIL import Image
 import typer
 
 
 def main(output_dir: Path, seed: int = 42):
-    for image in range(10):
-        Image.new("RGB", (10, 10)).save(output_dir / f"{image}.tif")
+    for chip in range(10):
+        chip_dir = output_dir / f"{chip:04d}"
+        chip_dir.mkdir(exist_ok=True, parents=True)
+        for band in range(4):
+            Image.fromarray(
+                np.random.randint(low=0, high=256, size=(10, 10)), mode="L"
+            ).save(chip_dir / f"B{band:02d}.tif")
 
 
 if __name__ == "__main__":
