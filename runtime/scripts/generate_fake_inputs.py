7c88b3a0cd0e5fb1a5dbe071c9bb67d83856abaf
Robert Gibboni <robert@drivendata.org>
2021-11-05 09:23:18 -0400

    Generate fake data w/o numpy

--- runtime/scripts/generate_fake_inputs.py
+++ runtime/scripts/generate_fake_inputs.py
@@ -1,6 +1,8 @@
+from functools import reduce
+from operator import mul
 from pathlib import Path
+from random import randint
 
-import numpy as np
 from PIL import Image
 import typer
 
@@ -10,9 +12,9 @@ def main(output_dir: Path, seed: int = 42):
         chip_dir = output_dir / f"{chip:04d}"
         chip_dir.mkdir(exist_ok=True, parents=True)
         for band in range(4):
-            Image.fromarray(
-                np.random.randint(low=0, high=256, size=(10, 10)), mode="L"
-            ).save(chip_dir / f"B{band:02d}.tif")
+            im = Image.new(mode="L", size=(10, 10))
+            im.putdata([randint(0, 255) for _ in range(reduce(mul, im.size))])
+            im.save(chip_dir / f"B{band:02d}.tif")
 
 
 if __name__ == "__main__":
