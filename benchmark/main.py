70e4cc79dd439fe5b746481f76a6e5b7f5d5d588
Robert Gibboni <robert@drivendata.org>
2021-11-08 10:09:10 -0500

    Benchmark that just predicts gt/lt the mean pixel value (#2)
    * Benchmark that just predicts gt/lt the mean pixel value
    
    * Update generate fake inputs to make tifs with single bands
    
    * Generate fake data w/o numpy
    
    * Ignore runtime sample data

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,44 +1,27 @@
 from pathlib import Path
-from urllib.request import urlopen
 
-import planetary_computer as pc
+from loguru import logger
+import numpy as np
 from PIL import Image
-from pystac_client import Client
 import typer
 
 ROOT_DIRECTORY = Path("/codeexecution")
 SUBMISSION_DIRECTORY = ROOT_DIRECTORY / "submission"
-DATA_DIRECTORY = ROOT_DIRECTORY / "data"
+
+feature_directory = ROOT_DIRECTORY / "data" / "test_features"
+
+chips = sorted(chip for chip in feature_directory.glob("*") if chip.is_dir())
+logger.info(f"Processing {len(chips)} chips in {feature_directory}")
 
 
 def main():
-    """Just check if we can call the Planetary Computer STAC API."""
-    catalog = Client.open("https://planetarycomputer.microsoft.com/api/stac/v1")
-    area_of_interest = {
-        "type": "Polygon",
-        "coordinates": [
-            [
-                [-122.27508544921875, 47.54687159892238],
-                [-121.96128845214844, 47.54687159892238],
-                [-121.96128845214844, 47.745787772920934],
-                [-122.27508544921875, 47.745787772920934],
-                [-122.27508544921875, 47.54687159892238],
-            ]
-        ],
-    }
-
-    search = catalog.search(
-        collections=["landsat-8-c2-l2"],
-        intersects=area_of_interest,
-        datetime="2020-12-01/2020-12-31",
-    )
-
-    items = list(search.get_items())
-    selected_item = sorted(items, key=lambda item: item.properties["eo:cloud_cover"])[0]
-    thumbnail_asset = selected_item.assets["thumbnail"]
-    signed_href = pc.sign(thumbnail_asset.href)
-
-    Image.open(urlopen(signed_href)).save(SUBMISSION_DIRECTORY / "thumb.tif")
+    for chip in chips:
+        images = np.array(
+            [np.array(Image.open(image)) for image in chip.glob("*.tif")]
+        ).mean(0)
+        Image.fromarray((images > images.mean()).astype(np.uint8)).save(
+            SUBMISSION_DIRECTORY / f"{chip.name}.tif"
+        )
 
 
 if __name__ == "__main__":
--- runtime/scripts/generate_fake_inputs.py
+++ runtime/scripts/generate_fake_inputs.py
@@ -1,12 +1,20 @@
+from functools import reduce
+from operator import mul
 from pathlib import Path
+from random import randint
 
 from PIL import Image
 import typer
 
 
 def main(output_dir: Path, seed: int = 42):
-    for image in range(10):
-        Image.new("RGB", (10, 10)).save(output_dir / f"{image}.tif")
+    for chip in range(10):
+        chip_dir = output_dir / f"{chip:04d}"
+        chip_dir.mkdir(exist_ok=True, parents=True)
+        for band in range(4):
+            im = Image.new(mode="L", size=(10, 10))
+            im.putdata([randint(0, 255) for _ in range(reduce(mul, im.size))])
+            im.save(chip_dir / f"B{band:02d}.tif")
 
 
 if __name__ == "__main__":
