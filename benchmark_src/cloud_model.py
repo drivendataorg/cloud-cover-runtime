1f4e0a87e40ec7949defdc7458050a08c0408029
Robert Gibboni <robert@drivendata.org>
2021-11-18 12:32:35 -0500

    Update benchmark model

--- benchmark_src/cloud_model.py
+++ benchmark_src/cloud_model.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from pathlib import Path
 import pytorch_lightning as pl
+import segmentation_models_pytorch as smp
 import torch
 from typing import Optional
 
 from cloud_dataset import CloudDataset
-from losses import XEDiceLoss, intersection_and_union
+from losses import intersection_over_union
 
 
 class CloudModel(pl.LightningModule):
@@ -45,53 +45,35 @@ class CloudModel(pl.LightningModule):
         # required
         self.bands = bands
 
-        # required for training
-        self.x_train = x_train
-        self.y_train = y_train
-        self.x_val = x_val
-        self.y_val = y_val
-
         # optional modeling params
+        self.backbone = self.hparams.get("backbone", "resnet34")
+        self.weights = self.hparams.get("weights", "imagenet")
         self.learning_rate = self.hparams.get("lr", 1e-3)
-        self.max_epochs = self.hparams.get("max_epochs", 1000)
-        self.min_epochs = self.hparams.get("min_epochs", 6)
         self.patience = self.hparams.get("patience", 4)
         self.num_workers = self.hparams.get("num_workers", 2)
         self.batch_size = self.hparams.get("batch_size", 32)
-        self.output_path = self.hparams.get("output_path", "model-outputs")
         self.gpu = self.hparams.get("gpu", False)
         self.transform = None
 
-        # Where final model will be saved
-        self.output_path = Path.cwd() / self.output_path
-        self.output_path.mkdir(exist_ok=True)
-
-        # Track validation IOU globally (reset each epoch)
-        self.intersection = 0
-        self.union = 0
-
         # Instantiate datasets, model, and trainer params if provided
-        if self.x_train is not None and self.y_train is not None:
-            self.train_dataset = CloudDataset(
-                x_paths=self.x_train,
-                bands=self.bands,
-                y_paths=self.y_train,
-                transforms=self.transform,
-            )
-        if self.x_val is not None and self.y_val is not None:
-            self.val_dataset = CloudDataset(
-                x_paths=self.x_val,
-                bands=self.bands,
-                y_paths=self.y_val,
-                transforms=None,
-            )
+        self.train_dataset = CloudDataset(
+            x_paths=x_train,
+            bands=self.bands,
+            y_paths=y_train,
+            transforms=self.transform,
+        )
+        self.val_dataset = CloudDataset(
+            x_paths=x_val,
+            bands=self.bands,
+            y_paths=y_val,
+            transforms=None,
+        )
         self.model = self._prepare_model()
-        self.trainer_params = self._get_trainer_params()
 
     ## Required LightningModule methods ##
 
     def forward(self, image: torch.Tensor):
-        """Forward pass"""
+        # Forward pass
         return self.model(image)
 
     def training_step(self, batch: dict, batch_idx: int):
@@ -103,8 +85,7 @@ class CloudModel(pl.LightningModule):
                 {'chip_id': list[str], 'chip': list[torch.Tensor], 'label': list[torch.Tensor]}
             batch_idx (int): batch number
         """
-        # make sure training data has been provided
-        if self.x_train is None or self.y_train is None:
+        if self.train_dataset.data is None:
             raise ValueError(
                 "x_train and y_train must be specified when CloudModel is instantiated to run training"
             )
@@ -123,18 +104,18 @@ class CloudModel(pl.LightningModule):
         preds = self.forward(x)
 
         # Calculate training loss
-        xe_dice_loss = XEDiceLoss()(preds, y)
+        xe_loss = torch.nn.CrossEntropyLoss(reduction="none")(preds, y).mean()
 
         # Log batch xe_dice_loss
         self.log(
-            "xe_dice_loss",
-            xe_dice_loss,
+            "xe_loss",
+            xe_loss,
             on_step=True,
             on_epoch=True,
             prog_bar=True,
             logger=True,
         )
-        return xe_dice_loss
+        return xe_loss
 
     def validation_step(self, batch: dict, batch_idx: int):
         """
@@ -145,8 +126,7 @@ class CloudModel(pl.LightningModule):
                 {'chip_id': list[str], 'chip': list[torch.Tensor], 'label': list[torch.Tensor]}
             batch_idx (int): batch number
         """
-        # make sure validation data has been provided
-        if self.x_val is None or self.y_val is None:
+        if self.val_dataset.data is None:
             raise ValueError(
                 "x_val and y_val must be specified when CloudModel is instantiated to run validation"
             )
@@ -166,13 +146,8 @@ class CloudModel(pl.LightningModule):
         preds = torch.softmax(preds, dim=1)[:, 1]
         preds = (preds > 0.5) * 1  # convert to int
 
-        # Calculate validation IOU (global)
-        intersection, union = intersection_and_union(preds, y)
-        self.intersection += intersection
-        self.union += union
-
         # Log batch IOU
-        batch_iou = intersection / union
+        batch_iou = intersection_over_union(preds, y)
         self.log(
             "iou", batch_iou, on_step=True, on_epoch=True, prog_bar=True, logger=True
         )
@@ -199,82 +174,27 @@ class CloudModel(pl.LightningModule):
         )
 
     def configure_optimizers(self):
-        # Define optimizer
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
-
-        # Define scheduler
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer, mode="max", factor=0.5, patience=self.patience
-        )
-        scheduler = {
-            "scheduler": scheduler,
-            "interval": "epoch",
-            "monitor": "val_loss",
-        }  # logged value to monitor
-        return [optimizer], [scheduler]
-
-    def validation_epoch_end(self, outputs: list[torch.Tensor]):
-        # Calculate IOU at end of epoch
-        epoch_iou = self.intersection / self.union
-
-        # Reset metrics before next epoch
-        self.intersection = 0
-        self.union = 0
-
-        # Log epoch validation IOU
-        self.log("val_loss", epoch_iou, on_epoch=True, prog_bar=True, logger=True)
-        return epoch_iou
+        opt = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        sch = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max=10)
+        return [opt], [sch]
 
     ## Convenience Methods ##
 
     def _prepare_model(self):
-        # FPN: https://github.com/AdeelH/pytorch-fpn
-        model = torch.hub.load(
-            "AdeelH/pytorch-fpn",
-            "make_fpn_resnet",
-            force_reload=True,
-            name="resnet18",
-            fpn_type="fpn",
-            num_classes=2,
-            fpn_channels=256,
-            in_channels=4,  # number of channels / bands
-            out_size=(512, 512),
+        # Instantiate U-Net model
+        unet_model = smp.Unet(
+            encoder_name=self.backbone,
+            encoder_weights=self.weights,
+            in_channels=4,
+            classes=2,
         )
-        return model
-
-    def _get_trainer_params(self):
-        # Define callback behavior
-        checkpoint_callback = pl.callbacks.ModelCheckpoint(
-            dirpath=self.output_path,
-            monitor="val_loss",
-            mode="max",
-            verbose=True,
-        )
-        early_stop_callback = pl.callbacks.early_stopping.EarlyStopping(
-            monitor="val_loss",
-            patience=(self.patience * 3),
-            mode="max",
-            verbose=True,
-        )
-
-        # Specify where TensorBoard logs will be saved
-        self.log_path = Path.cwd() / self.hparams.get("log_path", "tensorboard-logs")
-        self.log_path.mkdir(exist_ok=True)
-        logger = pl.loggers.TensorBoardLogger(self.log_path, name="benchmark-model")
-
-        trainer_params = {
-            "callbacks": [checkpoint_callback, early_stop_callback],
-            "max_epochs": self.max_epochs,
-            "min_epochs": self.min_epochs,
-            "default_root_dir": self.output_path,
-            "logger": logger,
-            "gpus": None if not self.gpu else 1,
-            "fast_dev_run": self.hparams.get("fast_dev_run", False),
-            "num_sanity_val_steps": self.hparams.get("val_sanity_checks", 0),
-        }
-        return trainer_params
+        if self.gpu:
+            unet_model.cuda()
+        return unet_model
 
     def fit(self):
         # Set up and fit Trainer object
-        self.trainer = pl.Trainer(**self.trainer_params)
+        trainer_params = self.hparams.get("trainer_params", {})
+        trainer_params.update({"gpus": None if not self.gpu else 1})
+        self.trainer = pl.Trainer(**trainer_params)
         self.trainer.fit(self)
