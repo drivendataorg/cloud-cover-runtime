785785f4e774bb10c0f71888131c2e414bd0ee3d
Robert Gibboni <robert@drivendata.org>
2021-11-30 15:32:12 -0500

    Fewer fake images, use random seed

--- runtime/scripts/generate_fake_inputs.py
+++ runtime/scripts/generate_fake_inputs.py
@@ -5,15 +5,17 @@ import pandas as pd
 from PIL import Image
 import typer
 
+rng = np.random.RandomState(seed=45)
 
-def main(output_dir: Path, seed: int = 42):
+
+def main(output_dir: Path):
     metadata = []
-    for chip in range(10):
+    for chip in range(4):
         chip_dir = output_dir / "test_features" / f"{chip:04d}"
         chip_dir.mkdir(exist_ok=True, parents=True)
         for band in ["B02", "B03", "B04", "B08"]:
             im = Image.new(mode="L", size=(512, 512))
-            im.putdata(np.random.randint(0, 256, size=(512, 512), dtype=np.uint8))
+            im.putdata(rng.randint(0, 256, size=(512, 512), dtype=np.uint8))
             im.save(chip_dir / f"{band}.tif")
         metadata.append(
             {
